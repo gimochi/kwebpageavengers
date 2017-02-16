4053b638a372816f09a8eb647fb47b6a52bc013e
jjwow73 <jongwow73@gmail.com>
2017-02-16 23:57:50 +0900

    mainpage css

--- forum/frontend/src/components/body_home_container.js
+++ forum/frontend/src/components/body_home_container.js
@@ -7,7 +7,7 @@ var BodyHomeContainer = React.createClass({
            <div id={this.props.container_name} className="col-md-12 body_home_container">
                <div className="body_home_title">
                    {this.props.title}
-                   <span id={this.props.more_name} className="body_home_more label label-default">+more</span>
+                   <a href="#"><span id={this.props.more_name} className="body_home_more label label-default">+more</span></a>
                </div>
                 <div className="body_home_content">
                 {/* 내용이 들어가야 함. */}
