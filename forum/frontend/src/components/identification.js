1ee115bbf25f933593f067c268240267e5248d15
vkfwlsdhs <dbwogy1@naver.com>
2017-02-23 19:38:49 +0900

    edit

--- forum/frontend/src/components/identification.js
+++ forum/frontend/src/components/identification.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router';
 
 var Identification = React.createClass({
+  getInitialState() {
+    return {
+
+     pw_fun:function(){ // 변경확인 버튼, 비밀번호 바꾸는 정보수정 부분
+        var pwd = $('#id_password').val();
+        var pwdc = $('#id_password_check').val();
+        if(pwd.length > 4 && pwd.length < 12 && pwdc.length > 4 && pwdc.length <12){
+          if(pwd != pwdc) {
+            alert('비밀번호확인이 같지 않습니다!');
+          }
+          else{
+            /*
+              비밀번호 변경해주는 코드
+              + 기존비밀번호랑 같은지 체크...
+            */
+            alert('변경 성공!');
+            history.go(0);
+          }
+        }
+        else{
+          alert('비밀번호는 5자이상 12자미만이어야 합니다.');
+        }
+     }
+    };
+   },
+
    render:function () {
       var userinfo = ["채호경", "은수저 (정회원)", "2015410019", "ckdwhzjarhk@naver.com", "ckdwhzjarhk"];
       return(
@@ -74,7 +100,7 @@ var Identification = React.createClass({
             </tbody>
           </table>
 
-          <button>
+          <button onClick={ this.state.pw_fun }>
             변경 완료
           </button>
         </div>
