13da6470d2d0f3e6c95a16eb337e80e45f03535d
jjwow73 <jongwow73@gmail.com>
2017-02-24 02:35:55 +0900

    [css]datgul list, userinfo

--- forum/frontend/src/components/datgul_list.js
+++ forum/frontend/src/components/datgul_list.js
@@ -4,27 +4,25 @@ import ReactDOM from 'react-dom';
 var DatgulList = React.createClass ({
     render: function () {
         return(
-			<div className="datgul_list">
-				<table className="datgul_list_table">
-					<tbody>
-						<tr>
-						<th>
-							<img src={this.props.content[0]} className="datgul_list_img" />
-						</th>
-						<td>
-							{this.props.content[1]}
-						</td>
-						<td>
-							{this.props.content[2]}
-						</td>
-						<td>
-							{this.props.content[3]}
-						</td>
-						</tr>
-					</tbody>
-				</table>
-				<br />
-				{this.props.content[4]}
+			<div className="datgul_list row">
+				{/* 사진경로 이름 등급 시간 */}
+				<div className="col-md-2 col-sm-2 col-xs-2">
+					<div className="datgul_face">
+					<img src={this.props.content[0]} className="img img-responsive img-circle" />
+					</div>
+				</div>
+				<div className="col-md-10 col-sm-10 col-xs-10">
+					<div className="row">
+						<div className="datgul_name pull-left">{this.props.content[1]}</div>
+						<div className="datgul_sujeo pull-left">{this.props.content[2]}</div>
+						<div className="datgul_time pull-right">{this.props.content[3]}</div>
+					</div>
+					<div className="row datgul_content">
+						<div className="col-md-12">
+							{this.props.content[4]}
+						</div>
+					</div>
+				</div>
 			</div>
         );
     }
--- forum/frontend/src/components/identification.js
+++ forum/frontend/src/components/identification.js
@@ -32,78 +32,137 @@ var Identification = React.createClass({
    render:function () {
       var userinfo = ["채호경", "은수저 (정회원)", "2015410019", "ckdwhzjarhk@naver.com", "ckdwhzjarhk"];
       return(
-        <div id="id_container">
-          <img src="" id="id_img">
-          </img>
-          <br />
-          <button>
-            프로필 사진 변경
-          </button>
-          <table id="id_table">
-            <tbody>
-              <tr>
-                <th>
-                  회원 이름
-                </th>
-                <td>
-                  {userinfo[0]}
-                </td>
-              </tr>
-              <tr>
-                <th>
-                  등급
-                </th>
-                <td>
-                  {userinfo[1]}
-                </td>
-              </tr>
-              <tr>
-                <th>
-                  학번
-                </th>
-                <td>
-                  {userinfo[2]}
-                </td>
-              </tr>
-              <tr>
-                <th>
-                  이메일
-                </th>
-                <td>
-                  {userinfo[3]}
-                </td>
-              </tr>
-              <tr>
-                <th>
-                  아이디
-                </th>
-                <td>
-                  {userinfo[4]}
-                </td>
-              </tr>
-              <tr>
-                <th>
-                  새 비밀번호
-                </th>
-                <td>
-                  <input type="password" id="id_password"></input>
-                </td>
-              </tr>
-              <tr>
-                <th>
-                  새 비밀번호 확인
-                </th>
-                <td>
-                  <input type="password" id="id_password_check"></input>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="container">
+              <div className="row">
+                  <div className="col-xs-12 col-sm-12 col-md-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 toppad">
+                      <div className="panel panel-default">
+                          <div className="panel-heading" role="tab">
+                              <h3 className="panel-title text-center">프로필</h3>
+                          </div>
+                          <div className="panel-body">
+                              <div className="profile">
+                                  <div className="profile-head">
+                                      <div className="col-sm-6">
+                                          <div align="center">
+                                          <img className="img-rounded img-responsive" src={"../static/data/gallery_temp/1.jpg"} alt="User Picture" >
+                                          </img>
+                                          </div>
+                                          <div className="text-center">얼굴 바꾸기</div>
+                                      </div>
+                                      <div className="col-sm-6">
+                                          <span><h3>{userinfo[0]}</h3></span>
+                                          <span><h4>{userinfo[1]}</h4></span>
+                                      </div>
+                                  </div>
+                                  <div className="profile-body">
+                                      <div className="col-sm-12">
+                                          <table className="table table-bordered">
+                                              <tbody>
+                                                  <tr>
+                                                      <th>회원 이름</th>
+                                                      <td>{userinfo[0]}</td>
+                                                  </tr>
+                                                  <tr>
+                                                      <th>등급</th>
+                                                      <td>{userinfo[1]}</td>
+                                                  </tr>
+                                                  <tr>
+                                                      <th>학번</th>
+                                                      <td>{userinfo[2]}</td>
+                                                  </tr>
+                                                  <tr>
+                                                      <th>이메일</th>
+                                                      <td>{userinfo[3]}</td>
+                                                  </tr>
+                                              </tbody>
+                                          </table>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              {/* 개인정보는 본인 것만 수정 가능*/}
+              <div className="row">
+                  <div className="col-xs-12 col-sm-12 col-md-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 toppad">
+                      <div className="panel-group" id="profile-edit" role="tablist"  aria-multiselectable="true">
 
-          <button onClick={ this.state.pw_fun }>
-            변경 완료
-          </button>
-        </div>
+                          <div className="panel panel-default">
+                              <div className="panel-heading" role="tab" id="edit-1">
+                                  <h3 className="panel-title"><a data-toggle="collapse" data-parent="#profile-edit" href="#collapse1" aria-expanded="true" aria-controls="collapse1">비밀번호변경</a></h3>
+                              </div>
+                              <div id="collapse1" className="panel-collapse collapse" role="tabpanel"  aria-labelledby="edit-1">
+                                  <div className="panel-body">
+                                      <div className="profile">
+                                          <div className="profile-body">
+                                              <div className="col-sm-12">
+                                                  <table className="table table-bordered">
+                                                      <tbody>
+                                                      <tr>
+                                                          <th>아이디</th>
+                                                          <td>{userinfo[4]}</td>
+                                                      </tr>
+                                                      <tr>
+                                                          <th>새 비밀번호</th>
+                                                          <td><input className="form-control" type="password" id="id_password"></input></td>
+                                                      </tr>
+                                                      <tr>
+                                                          <th>새 비밀번호 확인</th>
+                                                          <td><input className="form-control" type="password" id="id_password_check"></input></td>
+                                                      </tr>
+                                                      </tbody>
+                                                  </table>
+                                                  <button onClick={ this.state.pw_fun } className="btn pull-right">
+                                                      변경 완료
+                                                  </button>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="panel panel-default">
+                              <div className="panel-heading" role="tab" id="edit-2">
+                                  <h3 className="panel-title"><a data-toggle="collapse" data-parent="#profile-edit" href="#collapse2" aria-expanded="true" aria-controls="collapse2">개인정보수정</a></h3>
+                              </div>
+                              <div id="collapse2" className="panel-collapse collapse" role="tabpanel"  aria-labelledby="edit-1">
+                                  <div className="panel-body">
+                                      <div className="profile">
+                                          <div className="profile-body">
+                                              <div className="col-sm-12">
+                                                  <table className="table table-bordered">
+                                                      <tbody>
+                                                      <tr>
+                                                          <th>회원 이름</th>
+                                                          <td><input type="text" id="profile_name" value={userinfo[0]}></input></td>
+                                                      </tr>
+                                                      <tr>
+                                                          <th>학번</th>
+                                                          <td>{userinfo[2]}</td>
+                                                      </tr>
+                                                      <tr>
+                                                          <th>이메일</th>
+                                                          <td><input type="text" id="profile_email" value={userinfo[3]}></input></td>
+                                                      </tr>
+                                                      </tbody>
+                                                  </table>
+                                                  <button className="btn pull-left">
+                                                      프로필사진 바꾸기
+                                                  </button>
+                                                  <button className="btn pull-right">
+                                                      개인정보 변경
+                                                  </button>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
       );
    } 
 });
