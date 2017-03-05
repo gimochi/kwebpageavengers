042bbf8cf1b663047983ad57ebabfdbf920847d3
mskim <minsubkim94@gmail.com>
2017-03-05 21:51:46 +0900

    Add routes/files.js And Update routes/index.js

--- forum/backend/routes/index.js
+++ forum/backend/routes/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var board = require('./board');
 var users = require('./users');
+var files = require('./files');
 var router = express.Router();
 
 
 router.use('/board', board);
 router.use('/users', users);
+router.use('/files/', files)
 
 
 
